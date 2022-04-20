bda6895995b834a4cc4e9d99af03f7b09464c434
MTahaOzsahin <ozsahinm16@itu.edu.tr>
2022-04-21 01:57:17 +0300

    d2
    following tutorial

--- Assets/GameFolders/Scripts/Concretes/Controllers/PlayerControllers/PlayerController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/PlayerControllers/PlayerController.cs
@@ -7,17 +7,24 @@ namespace UdemyProjectTutorial3.Concretes.Controllers.PlayerControllers
     public class PlayerController : MonoBehaviour
     {
         Animator playerAnimator;
+        Rigidbody2D playerRigibody2D;
 
 
         float horizontal;
+        bool isJump;
+
+        public bool IsJumpAction => playerRigibody2D.velocity != Vector2.zero; // sýfýrdan büyükse true döner.
+
         private void Awake()
         {
             playerAnimator = GetComponent<Animator>();
+            playerRigibody2D = GetComponent<Rigidbody2D>();
         }
 
         private void Update()
         {
             GetAxis();
+            JumpAction();
         }
         private void FixedUpdate()
         {
@@ -36,6 +43,19 @@ namespace UdemyProjectTutorial3.Concretes.Controllers.PlayerControllers
             {
                 transform.localScale = new Vector2(Mathf.Sign(horizontal), 1f);
             }
+        }
+        void JumpAction()
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                isJump = true;
+            }
+            if (isJump)
+            {
+                playerRigibody2D.AddForce(Vector2.up * 350f);
+                isJump = false;
+            }
+            playerAnimator.SetBool("isJump", IsJumpAction);
         }
     }
 }
